3cf3e9d5c91391700ff35da4abf8c95327dc0949
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-01-23 23:43:11 +0700

    create beautyful loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const loading = () => {
   return (
-    <div style={{position:"absolute",inset:0}}>
-        <img src="https://thumbs.gfycat.com/GeneralUnpleasantApisdorsatalaboriosa-max-1mb.gif" alt="..." style={{width:"100%",height:"100%",objectFit:"cover"}}/>
+    <div style={{position:"absolute",inset:0,background:"white"}}>
+        <div style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)"
+        }}>
+            <img src="https://thumbs.gfycat.com/GeneralUnpleasantApisdorsatalaboriosa-max-1mb.gif" alt="..." style={{width:"100%",height:"100%",objectFit:"cover"}}/>
+        </div>
     </div>
   )
 };
--- src/pages/cardDetail/CardDetail.jsx
+++ src/pages/cardDetail/CardDetail.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const CardDetail = () => {
+    // const a = useParams() // {cardId: 123123}
+    const {cardId} = useParams()
+
     return (
-        <div>
-            product detail
+        <div style={{width:"100%",height:"100vh",display:"flex",alignItems:"center",justifyContent:"center"}}>
+            <h1 style={{fontFamily:"fantasy"}}>
+                Id Card : {cardId}
+            </h1>
         </div>
     )
 }
