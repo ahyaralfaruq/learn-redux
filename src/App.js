ddbab9e7ea009e30e7a7240ddbf881377b175a79
ahyaralfaruq <ahyaralfaruq97@gmail.com>
2022-07-21 17:44:31 +0700

    menghilangkan warning/error/bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/home/Home'
-import CardDetail from './pages/cardDetail/CardDetail'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import CardDetail from "./pages/cardDetail/CardDetail";
+import { About } from "./pages";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/cards/:cardId" element={<CardDetail />} />
-      </Routes>     
-    </div>
-  );
+   return (
+      <div className="App">
+         <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/cards" exact element={<Home />} />
+            <Route path="/about" exact element={<About />} />
+            <Route path="/cards/:cardId" element={<CardDetail />} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/pages/about/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const index = () => {
+   return (
+      <div>
+         <h3>About Us</h3>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            quidem nihil temporibus rerum eos. Modi assumenda quis voluptatum
+            veniam voluptatem ab. Beatae optio ad porro fugiat veniam nisi rerum
+            perspiciatis.
+         </p>
+      </div>
+   );
+};
+
+export default index;
